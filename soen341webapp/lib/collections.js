d6037396e60cda1a00a33a7ad95dc141db93171a
Arnav Bhardwaj <arnavrbhardwaj@gmail.com>
2017-09-21 00:59:35 -0400

    organized the files with some comments, layed out the structure for any css in the main.css file. Worked on making mongoDB work on the main page. Added the heading for testing purposes, created a test Ads collection, inserted the items from the cmd directly using meteor mongo, to the server, insert statments are commented under the collections.js file, under main.js function is added called Ads as it will locate the items from colletion and place it on the front page when testList templet runs in body.'

--- soen341webapp/lib/collections.js
+++ soen341webapp/lib/collections.js
@@ -1,1 +1,7 @@
 import { Mongo } from 'meteor/mongo';
+
+export const Ads = new Mongo.Collection("Ads"); // create the collection for test ads
+
+// use meteor mongo on cmd and performed from client side
+//db.Ads.insert({ text: "FirstItem-Bat", createdAt: new Date() });
+//db.Ads.insert({ text: "Ball", createdAt: new Date() });
