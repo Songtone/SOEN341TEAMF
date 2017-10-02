03afc12ddcf2871b46e5d8fa40049d4e469fe1c0
Darrin Fong <darrinfong@gmail.com>
2017-10-02 16:56:05 -0400

    Starting implementation of issue #16. Starting state, nothing has been modified yet.

--- soen341webapp/client/main.js
+++ soen341webapp/client/main.js
@@ -2,8 +2,6 @@ import { Template } from 'meteor/templating';
 import {Posts} from '../lib/collections.js'; // import the "table"
 import {Accounts} from 'meteor/accounts-base';
 
-
-
 //Accounts config
 Accounts.ui.config({
     passwordSignupFields:'USERNAME_ONLY'
@@ -13,21 +11,18 @@ import './main.html';
 
 Template.body.helpers({
     posts(){
-        return Posts.find({}); 
+        return Posts.find({});
     }
 });
-
-
 
 Template.addPost.events({
     'submit form': function(event, template) {
         event.preventDefault(); // prevent page reload
-        
+
         var userId = "USERNAME";
         var category = event.target.category.value;
         var title = event.target.title.value;
         var desc = event.target.desc.value;
-
 
         Posts.insert({
            userId,
@@ -36,14 +31,13 @@ Template.addPost.events({
            desc,
            createdAt: new Date()
         });
-        
+
         //clear form
         event.target.reset();
-        
+
         //close modal
         $('.modal').modal('close');
-        
+
         return false;
-        
     }
 });
