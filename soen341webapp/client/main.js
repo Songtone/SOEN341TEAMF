014a021b24cb950cfabd2795f9a3e7b001638047
mohghalayini <mohghalayini90@gmail.com>
2017-10-03 23:02:04 -0400

    added a working text field for subcategories
    this should solve issue #28

--- soen341webapp/client/main.js
+++ soen341webapp/client/main.js
@@ -1,36 +1,37 @@
 import { Template } from 'meteor/templating';
 import {Posts} from '../lib/collections.js'; // import the "table"
-import {Accounts} from 'meteor/accounts-base';
-
-
+import {Accounts} from 'meteor/accounts-base'; // Accounts-ui takes care of password protection.
+import './main.html';
 
 //Accounts config
 Accounts.ui.config({
     passwordSignupFields:'USERNAME_ONLY'
-})
+});
 
-import './main.html';
-
+// To obtain the posts fom the collections
 Template.body.helpers({
     posts(){
         return Posts.find({});
     }
 });
 
-
+//submit form will retrive data from user and insert into Post collection.
 Template.addPost.events({
     'submit form': function(event, template) {
         event.preventDefault(); // prevent page reload
+
+        var userId = "USERNAME"; // change this to actual username of the person.
+
         var category = event.target.category.value;
         if(category!=""){
-        var userId = "USERNAME";
         var title = event.target.title.value;
         var desc = event.target.desc.value;
-
+        var subcategory= event.target.subcategory.value;
 
         Posts.insert({
            userId,
            category,
+           subcategory,
            title,
            desc,
            createdAt: new Date()
@@ -43,10 +44,9 @@ Template.addPost.events({
         $('.modal').modal('close');
 
         return false;
-
     }
-    else{
-      alert("You have not chosen a category");
+    else {
+      alert("Please fill in all fields before you submit your want")
     }
-}
+  }
 });
