5c2debe316d57f02b03e68f91bcb102bdd9cfb60
Darrin Fong <darrinfong@gmail.com>
2017-10-03 21:59:54 -0400

    changed the implementation of search bar, it is now part of the 'posts' template. This allows user to refresh posts without submitting form and refreshing the page

--- soen341webapp/client/main.js
+++ soen341webapp/client/main.js
@@ -1,18 +1,44 @@
-import {Template} from 'meteor/templating';
-import {Posts} from '../lib/collections.js'; // import the "table"
-import {Accounts} from 'meteor/accounts-base';
+import { Template } from 'meteor/templating';
+import { Posts } from '../lib/collections.js'; // import the "table"
+import { Accounts } from 'meteor/accounts-base';
+import { Tracker } from 'meteor/tracker';
+import './main.html';
 
 //Accounts config
 Accounts.ui.config({
   passwordSignupFields:'USERNAME_ONLY'
 })
 
-import './main.html';
+//RegEx function to remove reformat search string
+RegExp.escape = function(s) {
+  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
 
-Template.body.helpers({
-  posts(){
-    var searchWords = document.getElementsByTagName("search")[0].value;
-    return Posts.find({"title": searchWords});
+Posts.search = function(query) {
+  console.log("looking for: " + query);
+  if(_.isEmpty(query))
+    return Posts.find({});
+
+  return Posts.find({
+    $or: [{'title': { $regex: RegExp.escape(query), $options: 'i' }},
+    {'desc': { $regex: RegExp.escape(query), $options: 'i' }}]
+  });
+};
+
+Template.posts.events({
+  'keypress input': function(event, template) {
+    if (event.which === 13) {
+      Session.set('postsSearchQuery', event.target.value);
+    }
+  }
+});
+
+Template.posts.helpers({
+  searchResults: function() {
+    return Posts.search(Session.get('postsSearchQuery'));
+  },
+  postsSearchQuery: function() {
+    return Session.get('postsSearchQuery');
   }
 });
 
