c777234063db468cd3f342c135d529112a21f203
Arnav Bhardwaj <arnavrbhardwaj@gmail.com>
2017-10-28 15:44:04 -0400

    created the logic for adding phonenumber
    phone number is now added into the userData and can be used to display for issue #84

--- soen341webapp/client/profile.js
+++ soen341webapp/client/profile.js
@@ -11,20 +11,22 @@ Template.addUserData.events({
         event.preventDefault(); // prevent page reload
 
         // store the user input form fields into specific variables
-        var userId = Meteor.user().username;
+        var userId = Meteor.user()._id;
         var firstName = event.target.firstName.value;
         var lastName = event.target.lastName.value;
         var city = event.target.city.value;
         var province= event.target.province.value;
+        var phoneNumber = event.target.phonenumber.value;
 
-        if(firstName!="" && lastName!="" && city!="" && province!=""){
+        if(firstName!="" && lastName!="" && city!="" && province!="" && phoneNumber!=""){
             //added the information of the user in to the collections, userdata to later display on profile card.
             UserData.insert({
                 userId,
                 firstName,
                 lastName,
                 city,
-                province
+                province,
+                phoneNumber
             });
             //clear form
             event.target.reset();
