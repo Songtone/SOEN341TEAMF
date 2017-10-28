d872174083ec9b05d9ee155c985da468930fb142
Arnav Bhardwaj <arnavrbhardwaj@gmail.com>
2017-10-28 15:39:07 -0400

    made some code quality adjustments
    fixed variable names to reflect sprint 4 grading criteria

--- soen341webapp/client/main.js
+++ soen341webapp/client/main.js
@@ -133,23 +133,24 @@ Template.posts.events({'click .edit-Post': function(){
 });
 // this function checks if the edited form is complete (no empty fields) then edits the original post
 Template.editPost.events ({'click .submit-edited-post': function(){
-  var EditTitle= $("#edittitle").val();
-  var EditSubCat= $("#editsubcategory").val();
-  var Editdesc= $("#editdesc").val();
-  var EditCat= $("#editcategory").val();
-  var EditId= $("#editID").val();
-  var EditTime=$("#editTime").val();
-  var EditUserID=$("#editUserID").val();
-  var Editlikes=$("#editlikes").val();
-  if(EditCat!="" && EditSubCat!="" && EditTitle!="" && Editdesc !=""){
+  var editTitle= $("#edittitle").val();
+  var editSubCat= $("#editsubcategory").val();
+  var editDesc= $("#editdesc").val();
+  var editCat= $("#editcategory").val();
+  var editId= $("#editID").val();
+  var editTime=$("#editTime").val();
+  var editUserID=$("#editUserID").val();
+  var editLikes=$("#editlikes").val();
+
+  if(editCat!="" && editSubCat!="" && editTitle!="" && editDesc !=""){
     if (confirm("Are you sure you want to edit this want?")){
-  Posts.update({ _id: EditId },{ title: EditTitle, desc: Editdesc, subcategory: EditSubCat, likes:Editlikes, category:EditCat, userId:EditUserID,createdAt:EditTime });
+  Posts.update({ _id: editId },{ title: editTitle, desc: editDesc, subcategory: editSubCat, likes:editLikes, category:editCat, userId:editUserID,createdAt:editTime });
   var editmodal= document.getElementById("editPost");
   editmodal.style.display = "none";
 }
 else {
-    var editmodal= document.getElementById("editPost");
-    editmodal.style.display = "none";
+    var editModal= document.getElementById("editPost");
+    editModal.style.display = "none";
 }
 }
   else {
