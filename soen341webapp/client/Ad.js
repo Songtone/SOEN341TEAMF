812f3aef466f1935573d7f543c6c371e03d95486
Arnav Bhardwaj <arnavrbhardwaj@gmail.com>
2017-11-08 23:41:52 -0500

    created the ad.js file with needed imports
    made the created the imports as these data will be nneded to create further methods for #129

---
+++ soen341webapp/client/Ad.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import { UserData } from '../lib/collections.js';
+import { Tracker } from 'meteor/tracker';
+import './main.html';
+import { Accounts } from 'meteor/accounts-base';
+import { Likes } from '../lib/collections.js';
+import { Posts } from '../lib/collections.js';
+
+
