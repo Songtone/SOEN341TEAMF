d13937f43c894e2f555bc49a7e251dc866ae79d7
JKukulsky <joshua.kukulsky@gmail.com>
2017-11-08 23:49:20 -0500

    Included Additional imports, and a helper function to retrieve Post data

--- soen341webapp/client/Ad.js
+++ soen341webapp/client/Ad.js
@@ -1,9 +1,26 @@
-import { Template } from 'meteor/templating';
-import { UserData } from '../lib/collections.js';
-import { Tracker } from 'meteor/tracker';
-import './main.html';
-import { Accounts } from 'meteor/accounts-base';
+/*import { Template } from 'meteor/templating';
+import { Posts } from '../lib/collections.js'; // import the "table"
 import { Likes } from '../lib/collections.js';
-import { Posts } from '../lib/collections.js';
+import { Wants } from '../lib/collections.js';
+import { UserData } from '../lib/collections.js'; // the collection "user info"
+import { Tracker } from 'meteor/tracker';
+import { Accounts } from 'meteor/accounts-base';
 
 
+// helper
+Template.post.helpers({
+     postInfo: function() {
+           //check if post is present
+           var isExsist = UserData.find({ userId: Meteor.userId()});
+
+           if(!isExsist){
+               alert("Please complete the Fill Profile");
+           }
+           else {
+               return UserData.find({userId: Meteor.userId()});
+           }
+
+        }
+
+});
+*/
