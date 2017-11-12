c04192238ec2909aba450cfcf3a248eacad27adf
Darrin Fong <darrinfong@gmail.com>
2017-11-12 12:59:08 -0500

    Changing dropdown from button form to <select> for easier implementation

--- soen341webapp/client/main.js
+++ soen341webapp/client/main.js
@@ -11,41 +11,12 @@ Accounts.ui.config({
   passwordSignupFields:'USERNAME_AND_EMAIL'
 });
 
-// Close the dropdown menu if the user clicks outside of it
-Template.posts.events({
-  'click .dropDownButton': function() {
-    document.getElementById("categoriesMenu").classList.toggle("show");
-  }
-});
-
-// Change selection for dropdown menu
-Template.posts.events({
-  'click .categoriesDropdownItem': function() {
-    document.getElementById("dDownButton").innerHTML = document.getElementById("dDownButton").innerHTML.replace(/.*/i, document.getElementsByClassName("categoriesDropdownItem").innerHTML);
-  }
-});
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropDownButton')) {
-    var dropdowns = document.getElementsByClassName("categoriesMenuContent");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
 //RegEx function to remove reformat search string
 RegExp.escape = function(s) {
   return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 };
 
 Posts.search = function(query) {
-
   const options = {sort: {likes: -1}}; // option for the find() function call, will sort the results in descending order according to likes
   if(_.isEmpty(query))
     return Posts.find({}, options); // return posts without query
@@ -129,6 +100,7 @@ Template.addPost.events({
      }
   }
 });
+
 
 Template.posts.events({
   'click .delete-post': function() {
