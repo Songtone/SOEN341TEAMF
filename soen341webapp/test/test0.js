b8bbf7b40b008bd024f1e1156c9ea3c32838ecf9
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-12 23:53:14 -0500

    setting up a test to add a want

--- soen341webapp/test/test0.js
+++ soen341webapp/test/test0.js
@@ -1,5 +1,4 @@
-// Random test
-
+/* Random test
 (function() {
   var assert = require("assert");
   var plus = function (one, two) {
@@ -11,3 +10,41 @@
     });
   });
 }).call(this);
+*/
+
+//Initializing sample variables for a want post
+var userID = "Nick Nic";
+var category = "Arts";
+var title = "Testing Drawing";
+var desc = "Testing if this works";
+var subCategory = "I forgot what the options for this are";
+var likes = 0;
+
+function addWant()
+{
+            event.preventDefault(); // prevent page reload
+            if (category != "" && subCategory != "" && title != "" && desc != "") {
+                if (confirm("Are you sure you want to create this want?")) {
+                    Posts.insert({
+                        userId,
+                        category,
+                        subCategory,
+                        title,
+                        desc,
+                        likes,
+                        createdAt: new Date()
+                    });
+                    //clear form
+                    event.target.reset();
+                    //close modal
+                    $('.modal').modal('close');
+                    return false;
+                }
+            }
+            else {
+                alert("Please fill in all fields before you submit your Want")
+            }
+        }
+    });
+
+}
