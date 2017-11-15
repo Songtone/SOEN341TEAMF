9dd0f16c21ca20f0298e010fcf2d3df240640c51
mohghalayini <mohghalayini90@gmail.com>
2017-11-14 20:51:46 -0500

    Fixed a bug where user couldn't neither delete nor edit his post on his profile page
    the bug is caused by the deletion of some fields form the editPost.html page
    and the unavailability of some libraries in the profile.js

--- soen341webapp/client/profile.js
+++ soen341webapp/client/profile.js
@@ -1,5 +1,7 @@
 import { Template } from 'meteor/templating';
 import { UserData } from '../lib/collections.js'; // the collection "user info"
+import{Posts} from '../lib/collections.js';
+import{Likes} from '../lib/collections.js';
 import { Tracker } from 'meteor/tracker';
 import './main.html';
 import { Accounts } from 'meteor/accounts-base';
